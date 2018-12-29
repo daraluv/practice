f991680b322b4fe265ffda380d0aae34030a2dbf
“daraluv” <709191430@qq.com>
2018-12-29 14:36:18 +0800

    tab组件

---
+++ dragcase/src/components/Tab/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+// import classnames from 'classnames';
+import './style.css'
+import TabBtn from '../TabBtn';
+
+class Tab extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.titleArr = this.props.titles || [];
+    this.contentArr = this.props.contents || [];
+
+    this.state = {
+        current: 0,
+        titleArr: this.titleArr,
+        contentArr: this.contentArr 
+    };
+  }
+
+  changeHandler() {
+    this.setState({
+      titleArr: this.titleArr,
+    });
+    console.log(this.state)
+  }
+
+  setNav = (index) => {
+    return index === this.state.current ? 'title active' : 'title';
+  }
+
+  setContent = (index) => {
+    return index === this.state.current ? 'contentItem active' : 'contentItem';
+  }
+
+
+  render() {
+
+    return (
+        <div className="tabContainer">
+          <ul className="tabNav">
+              {
+                this.titleArr.map((item,i) =>
+                  <li onClick={() => {this.setState({ current:i })}} className={this.setNav(i)} key={i}>
+                    {item}
+                  </li>
+                )
+              }
+          </ul>
+          <div className="tabContent">
+              {
+                this.contentArr.map((item,i) =>
+                  <div onClick={() => {this.setState({ current:i })}} className={this.setContent(i)} key={i}>
+                    {item}
+                  </div>
+                )
+              }
+          </div>
+          <TabBtn changeHandler={(e)=>this.changeHandler(e)} titleArr={this.titleArr}></TabBtn>
+      </div>
+    )
+  }
+}
+
+
+export default Tab;
---
+++ dragcase/src/components/TabBtn/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './style.css'
+
+
+class TabBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.titleArr = this.props.titleArr || [];
+    this.state = {
+      inputVal: '',
+      titleArr: this.titleArr,
+    };
+  }
+
+  addHandle = () => {
+    this.setState({
+      titleArr: this.titleArr.push('新标题')
+    });   
+  }
+
+  removeHandle = () => {
+    if(this.titleArr.length > 0) {
+      this.setState({
+        titleArr: this.titleArr.pop()
+      }); 
+    }   
+  }
+
+  getinputVal = (e) => {
+    this.setState({
+      inputVal : e.target.value,
+    })
+  };
+
+
+  setValueHandle = () => {
+    const {inputVal} = this.state;
+    console.log()
+    if(inputVal > 0 && inputVal <= this.titleArr.length) {
+      this.setState({
+        titleArr: this.titleArr.splice(inputVal-1, 1)
+      }); 
+    }else {
+      console.log(`第${inputVal}页不存在`);
+    }
+   
+  }
+
+
+  render() {
+    return (
+      <div className="btnGroup" onClick={this.props.changeHandler}>
+        <button onClick={(e)=>this.addHandle(e)}>添加一页</button>
+        <button onClick={(e)=>this.removeHandle(e)}>删除一页</button>
+        <div>
+           删除第<input type="tel" value={this.state.inputVal} onChange={this.getinputVal}/>页
+           <button onClick={this.setValueHandle}>确定</button>
+        </div>
+      </div>
+       
+    )
+  }
+}
+
+
+export default TabBtn;
