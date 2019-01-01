91a0003c7bf403eab594abd8ff59ad7625dab966
阳波 <1832064870@qq.com>
2019-01-01 15:53:04 +0800

    update

--- dragcase/src/components/TabDemo/Btn.jsx
+++ dragcase/src/components/TabDemo/Btn.jsx
@@ -2,19 +2,33 @@ import React from 'react';
 
 import './style.css';
 
-class Btns extends React.Component {
-  render() {
-    const { data, remove, switchTo } = this.props;
-    return (
-      <div className="btns_container">
-        {data.map((item, i) => (
-          <button key={i} onClick={() => switchTo(i)}>{item}</button>
-        ))}
+// class Btns extends React.Component {
+//   render() {
+//     const { data, remove, switchTo } = this.props;
+//     return (
+//       <div className="btns_container">
+//         {data.map((item, i) => (
+//           <button key={i} onClick={() => switchTo(i)}>{item}</button>
+//         ))}
 
-        <button onClick={() => remove(data.length)}>删除最后一页</button>
-      </div>
-    )
-  }
+//         <button onClick={() => remove(data.length)}>删除最后一页</button>
+//       </div>
+//     )
+//   }
+// }
+
+function Btns(props) {
+  const { data, remove, switchTo } = props;
+
+  return (
+    <div className="btns_container">
+      {data.map((item, i) => (
+        <button key={i} onClick={() => switchTo(i)}>{item}</button>
+      ))}
+
+      <button onClick={() => remove(data.length)}>删除最后一页</button>
+    </div>
+  )
 }
 
 export default Btns;
