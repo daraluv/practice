ac040ba23a1cf21fc936166603fa80c71aef3f3f
dengdan <2634373044@qq.com>
2019-01-18 18:32:47 +0800

    create demo

---
+++ tsDemo/src/interface/index.tsx
@@ -0,0 +1,10 @@
+interface LabelledValue {
+  label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+  console.log(labelledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
