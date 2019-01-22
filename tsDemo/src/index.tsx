15cc212a5c37e5ec76bff6edbfc3516fe1cb77b1
阳波 <1832064870@qq.com>
2019-01-22 15:56:00 +0800

    fix

--- tsDemo/src/index.tsx
+++ tsDemo/src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import Drag from './components/Drag';
 
 ReactDOM.render(
-  <App />,
+  <Drag />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
